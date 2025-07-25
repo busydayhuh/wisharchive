367a14e3a11b6e50d0cdc40d18aaa27c8fc623c5
busydayhuh <107801504+busydayhuh@users.noreply.github.com>
2025-07-25 18:11:46 +0300

    Добавлены описания к заголовкам страниц

--- src/features/dashboard/ui/layouts/DashboardLayout.tsx
+++ src/features/dashboard/ui/layouts/DashboardLayout.tsx
@@ -24,10 +24,22 @@ export type OutletContextType = ViewModeSwitchType & {
 };
 
 const DASHBOARD_HEADERS = {
-  [ROUTES.BOOKED]: "Забронированные желания",
-  [ROUTES.ARCHIVED]: "Архив желаний",
-  [ROUTES.SHARED]: "Совместные списки",
-  [ROUTES.BOOKMARKS]: "Избранные списки",
+  [ROUTES.BOOKED]: {
+    header: "Хочу подарить",
+    description: "Забронированные вами желания",
+  },
+  [ROUTES.ARCHIVED]: {
+    header: "Архив желаний",
+    description: "Ваши желания, которые уже исполнились",
+  },
+  [ROUTES.SHARED]: {
+    header: "Совместные списки",
+    description: "Приватные списки, доступные вам",
+  },
+  [ROUTES.BOOKMARKS]: {
+    header: "Избранные списки",
+    description: "Списки, которые вы добавили в закладки",
+  },
 };
 
 export function DashboardLayout() {
@@ -47,21 +59,30 @@ export function DashboardLayout() {
   const [searchString, setSearchString] = useState("");
 
   return (
-    <div className="flex flex-col gap-10 md:gap-12 mt-2 md:mt-4 px-2 md:px-0">
+    <div className="flex flex-col gap-6 md:gap-12 mt-2 md:mt-4 px-2 md:px-0">
       <div className="flex md:flex-row flex-col justify-between md:items-center gap-4 md:gap-6">
         {dashboardHeader ? (
-          <span className="mb-4 md:mb-0 max-w-xs font-semibold text-3xl md:text-4xl leading-8">
-            {dashboardHeader}
-          </span>
+          <div className="flex flex-col gap-1 md:gap-3 mb-2 md:mb-0 max-w-xs">
+            <span className="font-semibold text-3xl md:text-4xl leading-8">
+              {dashboardHeader.header}
+            </span>
+            <span className="text-muted-foreground text-xs md:text-sm">
+              {dashboardHeader.description}
+            </span>
+          </div>
         ) : isOwner ? (
-          <span className="mb-4 md:mb-0 max-w-xs font-semibold text-3xl md:text-4xl leading-8">
+          <span className="mb-2 md:mb-0 max-w-xs font-semibold text-3xl md:text-4xl leading-8">
             Мой дашборд желаний
           </span>
         ) : null}
 
-        <UserInfo {...dashboardUser} />
+        {!isMobile ? (
+          <UserInfo {...dashboardUser} />
+        ) : !isOwner ? (
+          <UserInfo {...dashboardUser} />
+        ) : null}
       </div>
-      <div className="flex flex-col gap-6 -mb-7 md:-mb-9 lg:pr-6">
+      <div className="flex flex-col gap-6 -mb-4 md:-mb-9 lg:pr-6">
         {isMobile && <Searchbar setSearchString={setSearchString} />}
         <div className="flex justify-between items-end gap-3 md:gap-5 w-full">
           <Navigation />
