0f711007c5faf70f5dc5c3529fcd6b25f49193d7
busydayhuh <107801504+busydayhuh@users.noreply.github.com>
2025-08-11 19:00:43 +0300

    Не использую хлебные крошки

--- src/features/breadcrumbs/index.tsx
+++
@@ -1,9 +0,0 @@
-import { usePaths } from "./model/usePaths";
-
-function Breadcrumbs() {
-  const crumbs = usePaths();
-  console.log("crumbs :>> ", crumbs);
-  return <div>Breadcrumbs</div>;
-}
-
-export default Breadcrumbs;
