783ec66c5aad6e7ce512ec3cbf346cfe660055a6
busydayhuh <107801504+busydayhuh@users.noreply.github.com>
2025-12-01 18:33:50 +0300

    WishGalleryCover в отдельный компонент

--- src/features/dashboard/ui/content/wishes/WishGalleryCard.tsx
+++ src/features/dashboard/ui/content/wishes/WishGalleryCard.tsx
@@ -1,19 +1,13 @@
-import { type WishRoles } from "@/features/collaborators";
 import { useWishcardMeta } from "@/features/dashboard/model/hooks/useWishcardMeta";
-import {
-  BookButton,
-  FormattedPrice,
-  QuickActions,
-  WishImage,
-} from "@/features/wish";
+import { FormattedPrice } from "@/features/wish";
 import { WishlistControls } from "@/features/wishlist-controls";
 import { cn } from "@/shared/lib/css";
 import { useIsMobile } from "@/shared/lib/react/useIsMobile";
-import type { LinkParams, WishDocumentType } from "@/shared/model/types";
-import { PriorityBadge, PRIVACY_ICONS } from "@/shared/ui/Badges";
+import type { WishDocumentType } from "@/shared/model/types";
+import { PriorityBadge } from "@/shared/ui/Badges";
 import OwnerAvatar from "@/shared/ui/OwnerAvatar";
-import { memo } from "react";
 import { Link } from "react-router";
+import { WishGalleryCover } from "./WishGalleryCover";
 
 function WishGalleryCard({ wish }: { wish: WishDocumentType }) {
   const { onBookedPage, onListPage, userRoles, linkParams, onEditWish } =
@@ -26,7 +20,7 @@ function WishGalleryCard({ wish }: { wish: WishDocumentType }) {
         "relative flex flex-col gap-2 mb-4 md:mb-8 overflow-hidden"
       )}
     >
-      <WishCover
+      <WishGalleryCover
         wish={wish}
         userRoles={userRoles}
         linkParams={linkParams}
@@ -79,70 +73,5 @@ function WishGalleryCard({ wish }: { wish: WishDocumentType }) {
     </div>
   );
 }
-
-const WishCover = memo(function WishCover({
-  wish,
-  onEditWish,
-  userRoles,
-  linkParams,
-  isMobile,
-}: {
-  wish: WishDocumentType;
-  onEditWish: () => void;
-  userRoles?: WishRoles;
-  linkParams: LinkParams;
-  isMobile: boolean;
-}) {
-  return (
-    <div className={cn("relative rounded-2xl overflow-hidden")}>
-      <Link to={linkParams.to} state={linkParams.state}>
-        <WishImage
-          wishId={wish.$id}
-          url={wish.imageURL}
-          alt={wish.title}
-          variant="gallery"
-          isBooked={wish.isBooked}
-        />
-      </Link>
-
-      {wish.wishlist?.isPrivate && isMobile && (
-        <div className="top-2 left-2 absolute flex justify-center items-center rounded-full w-6 h-6 overflow-clip">
-          {PRIVACY_ICONS.private}
-        </div>
-      )}
-
-      {userRoles && (
-        <div
-          className={cn(
-            "right-0 bottom-0 absolute flex justify-end has-only:justify-end gap-1 p-3 w-fit"
-          )}
-        >
-          {userRoles.isWishOwner ? (
-            <QuickActions
-              imageURL={wish.imageURL ?? undefined}
-              wishId={wish.$id}
-              triggerVariant="gallery"
-              side="top"
-              align="start"
-              isArchived={wish.isArchived}
-              title={wish.title}
-              onEditWish={onEditWish}
-              className="transition-all duration-300 show-actions"
-            />
-          ) : (
-            <BookButton
-              wishTitle={wish.title}
-              imageURL={wish.imageURL ?? undefined}
-              wishId={wish.$id}
-              triggerVariant="gallery"
-              isBooked={wish.isBooked}
-              isBookedByCurrentUser={userRoles?.isBooker}
-            />
-          )}
-        </div>
-      )}
-    </div>
-  );
-});
 
 export default WishGalleryCard;
---
+++ src/features/dashboard/ui/content/wishes/WishGalleryCover.tsx
@@ -0,0 +1,72 @@
+import type { WishRoles } from "@/features/collaborators";
+import { BookButton, QuickActions, WishImage } from "@/features/wish";
+import { cn } from "@/shared/lib/css";
+import type { LinkParams, WishDocumentType } from "@/shared/model/types";
+import { PRIVACY_ICONS } from "@/shared/ui/Badges";
+import { memo } from "react";
+import { Link } from "react-router";
+
+export const WishGalleryCover = memo(function WishGalleryCover({
+  wish,
+  onEditWish,
+  userRoles,
+  linkParams,
+  isMobile,
+}: {
+  wish: WishDocumentType;
+  onEditWish: () => void;
+  userRoles?: WishRoles;
+  linkParams: LinkParams;
+  isMobile: boolean;
+}) {
+  return (
+    <div className={cn("relative rounded-2xl overflow-hidden")}>
+      <Link to={linkParams.to} state={linkParams.state}>
+        <WishImage
+          wishId={wish.$id}
+          url={wish.imageURL}
+          alt={wish.title}
+          variant="gallery"
+          isBooked={wish.isBooked}
+        />
+      </Link>
+
+      {wish.wishlist?.isPrivate && isMobile && (
+        <div className="top-2 left-2 absolute flex justify-center items-center rounded-full w-6 h-6 overflow-clip">
+          {PRIVACY_ICONS.private}
+        </div>
+      )}
+
+      {userRoles && (
+        <div
+          className={cn(
+            "right-0 bottom-0 absolute flex justify-end has-only:justify-end gap-1 p-3 w-fit"
+          )}
+        >
+          {userRoles.isWishOwner ? (
+            <QuickActions
+              imageURL={wish.imageURL ?? undefined}
+              wishId={wish.$id}
+              triggerVariant="gallery"
+              side="top"
+              align="start"
+              isArchived={wish.isArchived}
+              title={wish.title}
+              onEditWish={onEditWish}
+              className="transition-all duration-300 show-actions"
+            />
+          ) : (
+            <BookButton
+              wishTitle={wish.title}
+              imageURL={wish.imageURL ?? undefined}
+              wishId={wish.$id}
+              triggerVariant="gallery"
+              isBooked={wish.isBooked}
+              isBookedByCurrentUser={userRoles?.isBooker}
+            />
+          )}
+        </div>
+      )}
+    </div>
+  );
+});
