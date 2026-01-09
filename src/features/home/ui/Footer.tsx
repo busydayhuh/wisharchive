fc70885d5799234d8de0eb870962c2b351e53b07
busydayhuh <107801504+busydayhuh@users.noreply.github.com>
2026-01-09 21:55:57 +0300

    github link в footer

--- src/features/home/ui/Footer.tsx
+++ src/features/home/ui/Footer.tsx
@@ -3,7 +3,15 @@ import { Policy } from "./Policy";
 export function Footer() {
   return (
     <footer className="z-300 flex flex-wrap justify-center md:justify-between gap-2 mt-10 md:mt-15 lg:mt-25 2xl:mt-30 px-2 md:px-5 py-4 w-full font-grandis text-muted-foreground text-xs">
-      <p>© 2025 Wish Archive</p>
+      <p>
+        <a
+          href="https://github.com/busydayhuh/wisharchive"
+          target="_blank"
+          className="hover:underline"
+        >
+          GitHub
+        </a>
+      </p>
       <Policy
         text="Политика конфиденциальности"
         className="justify-self-center hover:underline"
