36f0552f2894c85943db941786dae592b9925981
busydayhuh <107801504+busydayhuh@users.noreply.github.com>
2026-01-09 14:46:30 +0300

    Заглушки для отсутствия картинки в поиске

--- src/features/global-search/ui/Media.tsx
+++ src/features/global-search/ui/Media.tsx
@@ -18,19 +18,27 @@ export function Media({
       />
     );
   if (category === "wishes")
-    return (
+    return item.imageURL ? (
       <img
-        src={item.imageURL ?? undefined}
+        src={item.imageURL}
         alt={item.title?.[0] || ""}
         className="flex justify-center items-center bg-muted font-medium"
       />
+    ) : (
+      <div className="flex justify-center items-center bg-muted w-full h-full font-medium">
+        {item.title?.[0] || ""}
+      </div>
     );
   if (category === "wishlists")
-    return (
+    return item.wishes?.at(-1)?.imageURL ? (
       <img
         src={item.wishes?.at(-1)?.imageURL}
         alt={item.title?.[0] || ""}
         className="flex justify-center items-center bg-muted font-medium"
       />
+    ) : (
+      <div className="flex justify-center items-center bg-muted w-full h-full font-medium">
+        {item.title?.[0] || ""}
+      </div>
     );
 }
