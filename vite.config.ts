33daac7ebb0111a01e17f441ad89fd17cc8d3140
busydayhuh <107801504+busydayhuh@users.noreply.github.com>
2026-01-09 22:14:04 +0300

    uninstall rollup visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,12 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import { visualizer } from "rollup-plugin-visualizer";
-import { defineConfig, type PluginOption } from "vite";
+import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tsconfigPaths(),
-    tailwindcss(),
-    svgr(),
-    visualizer({
-      open: true,
-      filename: "stats.html",
-    }) as PluginOption,
-  ],
+  plugins: [react(), tsconfigPaths(), tailwindcss(), svgr()],
   base: "/wisharchive",
   build: {
     outDir: "dist",
